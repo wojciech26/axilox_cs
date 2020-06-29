03784b03bf00780eaf704fe6870c12f32a91542f
wojciech26 <61656163+wojciech26@users.noreply.github.com>
2020-06-30 00:34:39 +0200

    menu update

--- Axilox/Program.cs
+++ Axilox/Program.cs
@@ -6,7 +6,7 @@ namespace Axilox
     {
         static void Main(string[] args)
         {
-            int[,] gameBoard = new int[8, 8];
+            int[,] gameBoard = new int[9, 9];
             boardGeneration(gameBoard);
             ShowBoard(gameBoard);
         }
@@ -19,14 +19,32 @@ namespace Axilox
             Random rnd = new Random();
             int[,] gameBoard = board;
 
-            for (int i = 0; i < 8; i++)
+            
+            for ( int i = 0;i<1;i++)
             {
-                for (int j = 0; j < 8; j++)
+                for (int j = 0; j < 9; j++)
+                {
+                    gameBoard[i,j] = j;
+                }
+            }
+
+            for (int j = 0; j < 1; j++)
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    gameBoard[i, j] = i;
+                }
+            }
+
+
+            for (int i = 1; i < 9; i++)
+            {
+                for (int j = 1; j < 9; j++)
                 {          
                     gameBoard[i, j] = rnd.Next(1, 9);
-                    Console.Write(" {0} ", gameBoard[i, j]);
+                    //Console.Write(" {0} ", gameBoard[i, j]);
                 }
-                Console.WriteLine();
+                //Console.WriteLine();
             }
 
             return gameBoard;
@@ -36,16 +54,29 @@ namespace Axilox
         {
             int[,] gameBoard = board;
 
-            for (int i = 0; i < 8; i++)
+            for (int i = 0; i < 9; i++)
             {
-                for (int j = 0; j < 8; j++)
+                for (int j = 0; j < 9; j++)
                 {
+                    if (i == 0 || j == 0) { Console.ForegroundColor = ConsoleColor.Green; }
+                    else
+                    {
+                        Console.ResetColor();
+                    }
                     Console.Write(" {0} ", gameBoard[i, j]);
                 }
                 Console.WriteLine();
             }
         }
-
+        private static void PrintMenu()
+        {
+            Console.WriteLine("Avaible options: ");
+            Console.WriteLine("s - shut down");
+            Console.WriteLine("p - print GameBoard");
+            Console.WriteLine("1-8 - pick a number");
+            // ...
+            Console.Write("Choose action: ");
+        }
 
     }
 }
