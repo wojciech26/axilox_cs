db34529eb582275fcef71a90c9b70e070a5178da
wojciech26 <61656163+wojciech26@users.noreply.github.com>
2020-06-29 21:50:50 +0200

    board generating

--- Axilox/Program.cs
+++ Axilox/Program.cs
@@ -6,7 +6,46 @@ namespace Axilox
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] gameBoard = new int[8, 8];
+            boardGeneration(gameBoard);
+            ShowBoard(gameBoard);
         }
+
+
+
+
+        public static int[,] boardGeneration(int[,] board)
+        {
+            Random rnd = new Random();
+            int[,] gameBoard = board;
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {          
+                    gameBoard[i, j] = rnd.Next(1, 9);
+                    Console.Write(" {0} ", gameBoard[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            return gameBoard;
+        }
+
+        public static void ShowBoard(int[,] board)
+        {
+            int[,] gameBoard = board;
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Console.Write(" {0} ", gameBoard[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+
     }
 }
